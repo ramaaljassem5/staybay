dd3224beb34073277e4d72a620cd71c0b039c09c
nabilmahmah <nabiilsy33@gmail.com>
2025-11-26 11:06:43 +0300

    commit farsh

---
+++ cubits/locale/locale_state.dart
@@ -0,0 +1,13 @@
+class LocaleState {}
+
+class LocaleInitial extends LocaleState {}
+
+class LocaleAR extends LocaleState {
+  Map<String, dynamic> local;
+  LocaleAR({required this.local});
+}
+
+class LocaleEN extends LocaleState {
+  Map<String, dynamic> local;
+  LocaleEN({required this.local});
+}
